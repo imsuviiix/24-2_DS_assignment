4fd4b78f1aa03f82f8bd1178a13eb88e9d5b2e3b
imsuviiix <gydbs0925@naver.com>
2024-09-24 03:54:53 +0900

    edit report (especially CNN)

--- Week_01/CNN/ViT_for_finance/architectures/cnn_ta.py
+++ Week_01/CNN/ViT_for_finance/architectures/cnn_ta.py
@@ -4,19 +4,31 @@ from torch.nn import functional as F
 
 
 class CNN(nn.Module):
-    def __init__(self, num_classes=3):
+    def __init__(self, num_classes: int =3): 
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=65, out_channels=16, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5, stride=1, padding=2)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=1, padding=2)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        self.fc1 = nn.Linear(32 * 16 * 16, 32)  
-        self.fc2 = nn.Linear(32, num_classes)
+        self.dropout1 = nn.Dropout(p=0.3)
+        self.dropout2 = nn.Dropout(p=0.45)
+        self.fc1 = nn.Linear(64 * 16 * 16, 128)  
+        self.fc2 = nn.Linear(128, num_classes)
+        
 
     def forward(self, x):
-        x = F.interpolate(x, size=(64, 64))  
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 32 * 16 * 16)  
+        if x.dim() == 3:
+            x = x.unsqueeze(1)
+        elif x.dim() == 4:
+            if x.size(1) != self.conv1.in_channels:
+                x = x.permute(0, 3, 1, 2)
+
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = self.dropout1(x)
+        x = x.contiguous().view(x.size(0), -1)
         x = F.relu(self.fc1(x))
+        x = self.dropout2(x)
         x = self.fc2(x)
         return x
